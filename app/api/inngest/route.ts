a9ea6e55224ee2c03d95f35da908ef9cc670e5e9
rhit-bhuwalk <kushbhuwalka@gmail.com>
2025-05-30 12:47:37 -0700

    jakob pls check this out

--- app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest/client";
-import { assistantNetwork } from "@/lib/network";
-
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  networks: [assistantNetwork],
-});
